8fa51c64476b96a26fad58d3283ec35134c28b61
AdityaDomala <aditya.domala21@gmail.com>
2024-02-19 16:07:44 -0500

    modules

---
+++ features.js
@@ -0,0 +1,10 @@
+const gfName = "Dont know";
+export const gfName2 = "Dont know 2";
+export const gfName3 = "Dont know 3";
+
+
+// module.exports = gfName;
+
+export default gfName;
+// export {gfName2, gfName3};
+
